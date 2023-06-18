c376686b836277f8268c69df74381c99a2102218
İsa Karaman <77787550+isakaraman@users.noreply.github.com>
2023-06-18 19:12:54 +0300

    First Commit

---
+++ Assets/Codes/gameChecker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class gameChecker : MonoBehaviour
+{
+    public SpriteRenderer[] sprts;
+    public Text winText;
+    public randomColor[] randomColorScrpt;
+    public AudioSource sounds;
+    public Button btn;
+    void Update()
+    {
+        if (sprts[0].color==sprts[1].color)
+        {
+            winText.text = "Same Colors You Win!";
+
+            randomColorScrpt[0].winCheckBool = true;
+            randomColorScrpt[1].winCheckBool = true;
+
+            btn.gameObject.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)&& randomColorScrpt[0].winCheckBool==false)
+        {
+            sounds.Play();
+        }
+
+    }
+    public void buttonCheck()
+    {
+        SceneManager.LoadScene(0);
+        
+    }
+}
---
+++ Assets/Codes/randomColor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomColor : MonoBehaviour
+{
+    public SpriteRenderer sprt;
+    public bool winCheckBool = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)&&!winCheckBool)
+        {
+            random_Colors();
+        }
+        
+    }
+
+    void random_Colors()
+    {
+        int rnd = Random.Range(1, 6);
+
+        switch (rnd)
+        {
+            case 1:
+                sprt.color = Color.yellow;
+                break;
+            case 2:
+                sprt.color = Color.blue;
+                break;
+            case 3:
+                sprt.color = Color.green;
+                break;
+            case 4:
+                sprt.color = Color.red;
+                break;
+            case 5:
+                sprt.color = Color.gray;
+                break;
+        }
+    }
+}
